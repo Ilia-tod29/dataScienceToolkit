d232d38354d4a872b5ecdec0f7a66629509bf87a
Ilia Todorov <tilia@vmware.com>
2023-06-17 19:45:02 +0300

    authentication and routes done

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -9,8 +9,10 @@ import { AuthenticationService } from "../services/authentication.service";
 })
 export class AuthComponent implements OnInit {
 
-  @ViewChild('error') error: ElementRef | undefined;
+  @ViewChild('errorSignIn') errorSignInEl: ElementRef | undefined;
+  @ViewChild('errorSignUp') errorSignUpEl: ElementRef | undefined;
   isRegistered: boolean = false;
+  isLoading: boolean = false;
   signUpObj = {
     email: "",
     password: "",
@@ -28,25 +30,58 @@ export class AuthComponent implements OnInit {
   }
 
   toggleRegister() {
-
+    this.isRegistered = !this.isRegistered;
   }
 
-  submitSignUp(event: Event | undefined) {
-    console.log(event);
+  submitSignUp() {
+    console.log("sign up");
+    if (this.signUpObj.password !== this.signUpObj.repeatPassword) {
+      this.errorSignUpEl!.nativeElement.textContent = "Passwords do not match";
+      return;
+    }
+
+    if (this.signUpObj.password.length < 8 || !this.hasCapitalLetter(this.signUpObj.password)) {
+      this.errorSignUpEl!.nativeElement.textContent = "Password must have at least 8 symbols and a capital letter";
+      return;
+    }
+
+    this.authenticationService.signUp({
+      email: this.signUpObj.email,
+      password: this.signUpObj.password
+    }).subscribe({
+      next: (response) => {
+        this.authenticationService.validate(response);
+        this.router.navigate(['home'])
+      },
+      error: error => {
+        this.isLoading = false;
+        this.errorSignUpEl!.nativeElement.textContent = error.message.slice(10);
+      }
+    });
   }
 
-  submitSignIn(event: Event | undefined) {
-    // event?.preventDefault();
-    console.log(event);
-    console.log(this.signInObj);
+  submitSignIn() {
+    console.log("sign in");
+    this.isLoading = true;
 
     this.authenticationService.signIn({
       email: this.signInObj.email,
       password: this.signInObj.password
-    }).subscribe(() => {
-      this.router.navigate(['home']);
+    }).subscribe({
+      next: (response) => {
+        this.authenticationService.validate(response);
+        this.router.navigate(['home']);
+      },
+      error: error => {
+        this.isLoading = false;
+        this.errorSignInEl!.nativeElement.textContent = error.message.slice(10);
+      }
     });
+  }
 
-    console.log("debaa");
+
+  private hasCapitalLetter(input: string): boolean {
+    const capitalLetterRegex = /[A-Z]/;
+    return capitalLetterRegex.test(input);
   }
 }
