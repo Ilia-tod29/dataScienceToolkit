b22af76a64c055efbc5e338f9c6dbdb076e15c8e
AlexPetrov <aleksandar2001_1995@abv.bg>
2023-06-18 03:11:17 +0300

    Move charts visualization

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  showPieChart: boolean = false;
+  pieChartStatus: string = "Show Pie Chart";
+  showLineChart: boolean = false;
+  lineChartStatus: string = "Show Line Chart";
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleShowPieChart(): void{
+    if (!this.showLineChart)
+    this.showPieChart = !this.showPieChart;
+
+    this.pieChartStatus = !this.showPieChart ? "Show Pie Chart" : "Hide";
+  }
+
+  toggleShowLineChart(): void {
+    if (!this.showPieChart)
+      this.showLineChart = !this.showLineChart;
+
+    this.lineChartStatus = !this.showLineChart ? "Show Line Chart" : "Hide";
   }
 
 }
